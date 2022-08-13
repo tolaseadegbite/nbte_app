45f0f22d852ed356682b388dda3cf3078e50915e
tolaseadegbite <tolasekelvin@gmail.com>
2022-08-13 03:29:50 +0100

    Setup basic homepage and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "static_pages#home"
+  devise_for :users
+  devise_scope :user do
+    get 'users', to: 'devise/sessions#new'
+  end
 end
